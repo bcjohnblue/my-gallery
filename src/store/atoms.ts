d09f29e8e4814b7e60617aa404424e07786c1f30
bcjohn <bcjohnblue@gmail.com>
2021-04-27 23:02:50 +0800

    Remove some components

--- src/store/atoms.ts
+++ src/store/atoms.ts
@@ -2,9 +2,8 @@
 import { atom } from 'recoil';
 import { PointerLockControls } from '@react-three/drei';
 
-// export const controlState = atom<React.RefObject<PointerLockControls> | null>({
 export const controlState = atom<PointerLockControls | null>({
-  key: 'controls', // unique ID (with respect to other atoms/selectors)
-  default: null, // default value (aka initial value)
+  key: 'controls',
+  default: null,
   dangerouslyAllowMutability: true,
 });
